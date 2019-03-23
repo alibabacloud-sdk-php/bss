e2e0ad836e416b9b4e85d54ac4ad9d7a722c12cc
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-23 17:03:28 +0800

    Update Apis

--- V20140714/Api.php
+++ V20140714/Api.php
@@ -2,7 +2,27 @@
 
 namespace AlibabaCloud\Bss\V20140714;
 
+use AlibabaCloud\ApiResolverTrait;
 use AlibabaCloud\Rpc;
+
+/**
+ * Resolve Api based on the method name.
+ *
+ * @method OpenCallback openCallback(array $options = [])
+ * @method QueryForCssOrder queryForCssOrder(array $options = [])
+ * @method CreateOrder createOrder(array $options = [])
+ * @method VnoPayCallBackNotify vnoPayCallBackNotify(array $options = [])
+ * @method VnoBatchRefundOrder vnoBatchRefundOrder(array $options = [])
+ * @method SubscriptionCreateOrderApi subscriptionCreateOrderApi(array $options = [])
+ * @method DescribeCashDetail describeCashDetail(array $options = [])
+ * @method SetResourceBusinessStatus setResourceBusinessStatus(array $options = [])
+ * @method DescribeCouponList describeCouponList(array $options = [])
+ * @method DescribeCouponDetail describeCouponDetail(array $options = [])
+ */
+class BssApiResolver
+{
+    use ApiResolverTrait;
+}
 
 class V20140714Rpc extends Rpc
 {
